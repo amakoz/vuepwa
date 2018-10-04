b87861ee9f913c09bd1bebb7498493a9ac596e88
amadeuszkozlowski <amadeuszkozlowski@gmail.com>
2018-10-04 02:17:09 +0200

    meta tags

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,6 +13,13 @@
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet" type="text/css">
     <link rel="manifest" href="/manifest.json">
 
+    <!-- Chrome, Firefox OS and Opera -->
+    <meta name="theme-color" content="#000000">
+    <!-- Windows Phone -->
+    <meta name="msapplication-navbutton-color" content="#000000">
+    <!-- iOS Safari -->
+    <meta name="apple-mobile-web-app-status-bar-style" content="#000000">
+
 </head>
 <body>
 <div class="container py-8" id="app">
